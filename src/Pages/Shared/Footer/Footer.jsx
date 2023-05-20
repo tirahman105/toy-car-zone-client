82f26c457bd41b050528cce6abe213169cb3db1e
tirahman105 <tirahman105@gmail.com>
2023-05-20 09:43:18 +0600

    footer update

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -3,27 +3,34 @@ import logo from '../../../assets/logo.png'
 
 const Footer = () => {
     return (
-        <div>
+        <div className='mt-4'>
             <footer className="footer p-10 bg-base-200 text-base-content">
   <div>
-    <span className="footer-title">Services</span> 
-    <a className="link link-hover">Branding</a> 
-    <a className="link link-hover">Design</a> 
-    <a className="link link-hover">Marketing</a> 
-    <a className="link link-hover">Advertisement</a>
+    <span className="footer-title">Pages</span> 
+  <li className="text-orange-700"><Link to='/'>Home</Link></li>
+    <li className="text-orange-700"><Link to='/alltoys'>All Toys</Link></li>
+    <li className="text-orange-700"><Link  to='/addtoy'>Add New Toy</Link></li>
+  
+   
   </div> 
   <div>
-    <span className="footer-title">Company</span> 
-    <a className="link link-hover">About us</a> 
-    <a className="link link-hover">Contact</a> 
-    <a className="link link-hover">Jobs</a> 
-    <a className="link link-hover">Press kit</a>
+    <span className="footer-title">Blog</span> 
+    <p>
+
+    <li className="text-orange-700"><Link to='/blogs'>Blog</Link></li>
+    </p>
   </div> 
   <div>
-    <span className="footer-title">Legal</span> 
-    <a className="link link-hover">Terms of use</a> 
-    <a className="link link-hover">Privacy policy</a> 
-    <a className="link link-hover">Cookie policy</a>
+    <span className="footer-title">Newsletter</span> 
+    <div className="form-control w-80">
+      <label className="label">
+        <span className="label-text">Enter your email address</span>
+      </label> 
+      <div className="relative">
+        <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" /> 
+        <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
+      </div>
+    </div>
   </div>
 </footer> 
 <footer className="footer px-10 py-4 border-t bg-base-200 text-base-content border-base-300">
